1f874b78e9d5f9443bdc570d53a851241eaa10c2
Linth84 <villalbaed84@gmail.com>
2026-08-24 09:55:44 -0300

    fix commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // GitHub Pages:
-  // https://linth84.github.io/discord-time/
-  base: '/discord-time/',
+  base: '/discord-time-app/',
 })
